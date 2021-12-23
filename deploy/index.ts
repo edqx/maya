db3ca9be81a329a510fa0d84d04e6b6ee73f7574
Edward Smale <essmale2005@gmail.com>
2021-12-23 17:01:30 +0000

    Fix signature verification and add logging

--- deploy/index.ts
+++ deploy/index.ts
@@ -22,7 +22,11 @@ function runCommandInDir(command: string, dir?: string) {
 
 const app = express();
 
-app.use(express.raw());
+app.use(express.raw({
+    inflate: true,
+    limit: "100kb",
+    type: "*/*"
+}));
 
 app.post("/", async (req, res) => {
     if (!req.header("User-Agent")?.startsWith("GitHub-Hookshot/")) {
@@ -31,38 +35,61 @@ app.post("/", async (req, res) => {
         return;
     }
 
-    const hmacVerifier = crypto.createHmac("sha256", process.env.WEBHOOK_SECRET as string);
-    hmacVerifier.write(req.body);
+    const hmacVerify = crypto.createHmac("sha256", process.env.WEBHOOK_SECRET as string);
+    hmacVerify.write(req.body);
+    const computedSha256 = hmacVerify.digest();
 
-    const sha256 = hmacVerifier.digest("hex");
+    const receivedSha256Str = req.header("X-Hub-Signature-256")?.split("=")[1];
 
-    if (sha256 !== req.header("X-Hub-Signature-256")) {
+    if (!receivedSha256Str) {
+        console.log("Got deploy POST but it wasn't signed:");
+        console.log("    " + Object.keys(req.headers).join(", "));
+        return;
+    }
+
+    const receivedSha256 = Buffer.from(receivedSha256Str, "hex");
+
+    if (crypto.timingSafeEqual(computedSha256, receivedSha256)) {
         console.log("Got deploy POST but the signature did not match:");
-        console.log("    " + sha256);
-        console.log("    " + req.header("X-Hub-Signature-256"));
+        console.log("    " + computedSha256.toString("hex"));
+        console.log("    " + receivedSha256.toString("hex"));
         return;
     }
 
     try {
         const json = JSON.parse(req.body.toString("utf8"));
+        
+        console.log("Got valid deploy POST, deploying..");
 
+        console.log("Getting current commit..");
         const currentCommitUntrimmed = await runCommandInDir("git rev-parse HEAD");
         const currentCommit = currentCommitUntrimmed.trim();
+        
+        console.log("    " + currentCommit);
     
         if (currentCommit === json.after) {
             console.log("Got deploy POST but the commit was the same as the last");
             return;
         }
 
+        console.log("Resetting any changes made locally..");
         await runCommandInDir("git reset --hard HEAD");
+        console.log("Pulling remote changes..");
         await runCommandInDir("git pull");
 
+        console.log("Building @maya/api..");
         await runCommandInDir("yarn build", path.resolve(process.cwd(), "..", "api"));
+        console.log("Building @maya/database..");
         await runCommandInDir("yarn build", path.resolve(process.cwd(), "..", "database"));
+        console.log("Building @maya/deploy..");
         await runCommandInDir("yarn build", path.resolve(process.cwd(), "..", "deploy"));
+        console.log("Building @maya/web..");
         await runCommandInDir("yarn build", path.resolve(process.cwd(), "..", "web"));
         
+        console.log("Restarting pm2 processes..");
         await runCommandInDir("pm2 restart all");
+
+        console.log("Success!");
     } catch (e) {
         console.log(e);
 
